b8ef36e5d61831abb1a4d6c00e10a65488e6372e
Mihail Pavlov <winni_ne@mail.ru>
2018-12-09 22:41:13 +0300

    admin logout button

--- src/admin/dashboard/index.js
+++ src/admin/dashboard/index.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
 import { CreateProduct } from './createProduct'
-import {ProductList} from './productList'
+import { ProductList } from './productList'
+import { Button } from 'evergreen-ui'
+import { connect } from 'react-redux'
+import { logoutUser } from '../../index/user/actions'
 
 const Grid = styled.div`
   display: grid;
   grid-template-columns: 300px calc(100vW - 300px);
-  grid-auto-rows: 500px;
+  grid-template-rows: 30px 500px;
+  grid-template-areas: "header header" "body body";
+`
+const Header = styled.header`
+  text-align: right;
+  grid-area: header;
 `
 
-export const Dashboard = () => {
+const dashboard = ( { logoutUser } ) =>{
+  const handleClick = () => {
+    logoutUser()
+  }
   return (
     <Grid>
-      <CreateProduct />
+      <Header> <Button onClick={ handleClick }>Logout</Button> </Header>
+      
+      <CreateProduct/>
       <ProductList/>
     </Grid>
   )
 }
+
+const mapDispatchToProps = {
+  logoutUser
+}
+
+export const Dashboard = connect(null, mapDispatchToProps)(dashboard)
